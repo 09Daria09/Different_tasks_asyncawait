a2aa6f4b8314a3aabde2315951fcc0fa24d1a0d1
Daria <dashakukuruza86@gmail.com>
2023-04-21 22:06:34 +0300

    Add project files.

---
+++ Different_tasks(async_await)/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Different_tasks_async_await_
+{
+    public partial class Form1 : Form
+    {
+        Task6 task6 = null;
+        Task7 task7 = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            task6 = new Task6();
+            task6.Show();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            task7 = new Task7();
+            task7.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button6_Click(object sender, EventArgs e)
+        {
+            await Asynk_Await();
+        }
+        private async Task Asynk_Await()
+        {
+            if(task6 != null)
+            {
+               await task6.FinalCount();
+            }
+            if (task7 != null)
+            {
+                await task7.FinalCount();
+            }
+        }
+
+    }
+}
---
+++ Different_tasks(async_await)/Task8.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Different_tasks_async_await_
+{
+    public partial class Task8 : Form
+    {
+        public Task8()
+        {
+            InitializeComponent();
+        }
+    }
+}
